86c837526809f93013fa7f6a8ad96a51b297a1d0
Jorge Gomez <gomerudo@gmail.com>
2018-11-09 16:08:45 +0100

    Preparing setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
-print("TODO")
+"""The setup.py file for auto-ml package."""
+
+import os
+from setuptools import setup
+
+
+setup(
+    name="auto-ml",
+    version="0.0.1",
+    author="Jorge Gomez Robles; Firoz Ansari",
+    author_email="j.gomez.robles@student.tue.nl",
+    description=("A package to solve classification and regression problems \
+using AutoML approaches."),
+    keywords="automl machine learning",
+    url="https://gomerudo.github.io/auto-ml/",
+    packages=['automl'],
+)
