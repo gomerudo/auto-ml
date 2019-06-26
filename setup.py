14a0379587ddeceefcecde3e68ec24eae6fbc968
Jorge Gomez <gomerudo@gmail.com>
2019-06-26 19:22:45 +0200

    Test the setup

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
-print("TODO")
+"""Setup the nasgym pkg with setuptools."""
+
+from setuptools import setup, find_packages
+
+setup(
+    # For installation
+    name='automl',
+    version='0.0.1',
+    install_requires=['tpot', 'auto-sklearn'],
+    packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
+
+    # Metadata to display on PyPI
+    author="Jorge Gomez Robles",
+    author_email="j.gomezrb.dev@gmail.com",
+    description="An AutoML framework",
+    license="MIT",
+    keywords="auto-ml machine-learning smac tpot",
+    url="https://gomerudo.github.io/auto-ml",
+)
